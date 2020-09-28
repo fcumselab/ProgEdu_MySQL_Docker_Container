dae1e8b9c40ce1c7874bb14f0a2e38338ddccfdf
FuFu77 <yachi@mail.fcu.edu.tw>
2020-09-28 19:09:45 +0800

    db table user add gitLabUsername

--- initial.sql
+++ initial.sql
@@ -649,6 +649,7 @@ CREATE TABLE `User` (
   `email` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `gitLabToken` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `display` tinyint(1) DEFAULT NULL,
+  `gitLabUsername` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
